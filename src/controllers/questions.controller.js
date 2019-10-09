d65293ba7e9c7a99eadd435519b21980200b0893
Peace <oyedejipeace@gmail.com>
2019-10-09 16:29:17 +0100

    create endpoints tto subscribe to a question create controller for routes write test to test for functionality of implementation

--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -1,6 +1,6 @@
 import {
   addQuestion, fetchAllQuestions, questionCount, fetchQuestion,
-  fetchVote, upVote, votesCount, downVote
+  fetchVote, upVote, votesCount, downVote, fetchSubscription, subscribe
 } from '../services/questions.service';
 
 
@@ -230,6 +230,70 @@ export const downVoteQuestion = async (req, res) => {
       data: {
         message: 'You downvoted this question',
         votes_count: numOfVotes
+      }
+    });
+  } catch (error) {
+    return res.status(502).send({
+      message: 'An error occurred'
+    });
+  }
+};
+
+/**
+* @export
+* @function subscribeQuestion
+* @param {Object} req - request received
+* @param {Object} res - response object
+* @returns {Object} JSON object (JSend format)
+*/
+export const subscribeQuestion = async (req, res) => {
+  try {
+    const {
+      params: {
+        questionId
+      },
+      user: {
+        _id: userId
+      }
+    } = req;
+
+    const question = await fetchQuestion(questionId)
+
+    if (!question) {
+      return res.status(404).send({
+        status: 'fail',
+        data: {
+          message: 'Question does not exist',
+        }
+      });
+    }
+
+    if (String(question.userId) === String(userId)) {
+      return res.status(401).send({
+        status: 'fail',
+        data: {
+          message: 'You cannot subscribe to your question',
+        }
+      });
+    }
+
+    const subcription = await fetchSubscription(questionId, userId)
+
+    if (subcription) {
+      return res.status(400).send({
+        status: 'fail',
+        data: {
+          message: 'You already subscribed to this question',
+        }
+      });
+    }
+
+    await subscribe(questionId, userId);
+
+    return res.status(200).send({
+      status: 'success',
+      data: {
+        message: 'You have subscribed to this question'
       }
     });
   } catch (error) {
